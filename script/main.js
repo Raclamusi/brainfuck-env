e37a9d34bf1af7a07ec1dbcbbc1f63c897159c7d
raclamusi <raclamusi@gmail.com>
2022-02-20 18:31:12 +0900

    UIを作った

--- script/main.js
+++ script/main.js
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", _event => {
     const editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
         mode: "brainfuck",
         lineNumbers: true,
+        tabSize: 4,
         indentUnit: 4,
+        indentWithTabs: true,
+        matchBrackets: true,
+        autoCloseBrackets: true,
     });
 });
